383723841f0f93c413e97f1180dcc31bcdf3e438
Meenu5 <ravisankarmeenakshi@gmail.com>
2019-02-11 10:31:44 +0530

    discussion

---
+++ pa1.py
@@ -0,0 +1,14 @@
+# 1a) get prior mean of 0,1,2 classes
+# assume normal distribution. find mean of x1, x2. Find variance also. (x-mu)2/n for each class
+# see 3.4.3 for multivariate and covariance given case
+
+# Doubt :
+# 1. Averaging over 20 replications ?
+
+
+
+# git clone git@github.com:Meenu5/PRML.git
+# # whatever files can be added
+# git add .
+# git commit -m "bla bla"
+# git push -u origin master
