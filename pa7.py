e56c58824ffb61c8c82cc1841ba29fac954326a1
avlakshmy <avlakshmy@gmail.com>
2019-03-10 17:15:56 +0530

    almost done but need to add rms error and plots

--- pa7.py
+++ pa7.py
@@ -9,25 +9,56 @@ random.seed(42)
 colors = (0,0,0)
 area = np.pi*3
 
-data = np.genfromtxt("../Datasets_PRML_A1/train100.txt", delimiter=',', dtype=None, encoding=None)
-data = [list(np.fromstring(data[i], dtype=float, count=3, sep=' ')) for i in range(len(data))]
+def getPolyfeatures(X,n) :
+    return np.squeeze(np.transpose(np.array([X**i for i in range(n+1)])))
 
+def getPolyfit(X,w) :
+    return np.matmul(X,w)
+
+def getAllPolyfit(X,w) :
+    polys = []
+    for i in range(X.shape[0]):
+        polys.append(getPolyfit(X[i], w))
+    return np.array(polys)
+
+def getWeights(X,y,ridge) :
+    return np.matmul(np.matmul(np.linalg.inv(ridge*np.eye(X.shape[1]) + np.matmul(np.transpose(X),X)),np.transpose(X)),y)
+
+def getAllWeights(X,y,ridge) :
+    weights = []
+    for i in range(X.shape[0]):
+        weights.append(getWeights(X[i], y, ridge))
+    return np.array(weights)
+
+def getEmpiricalRisk(y_train, y_pred):
+    diff = y_train - y_pred
+    sqd = np.squeeze(np.array([x*x for x in diff]))
+    return sum(sqd)
+
+data = np.loadtxt("../Datasets_PRML_A1/train100.txt", delimiter=' ', dtype=None)
+
+X_train = data[:,:2]
+y_train = data[:,-1]
 w = []
-# find w using (phi(x)' * phi(x))^-1 * phi(x)' * y
-# plot to show the overfitting
+X_poly = getPolyfeatures(X_train, 3)
+w = getAllWeights(X_poly, y_train, 0)
+y_pred = getAllPolyfit(X_poly, np.transpose(w))
+# compute rms error
 
 # controlling the overfitting by using Ridge Regression
 lambdas = [0.0001, 0.001, 0.01, 0.1, 1, 10]
-# find w taking lambda into account and plot
-# for lb in lambdas:
-#     plt.scatter(terget_values, predicted_values, s=area, c=colors, alpha=0.5)
-#     plt.title('Scatter plot for lambda = %f' %(lb))
-#     plt.xlabel('target')
-#     plt.ylabel('predicted')
-#     plt.show()
+for lb in lambdas:
+    w = getAllWeights(X_poly, y_train, lb)
+    y_pred = getAllPolyfit(X_poly, np.transpose(w))
+    # compute rms error
 
 # controlling the overfitting by increasing the training data size
-data_1 = np.genfromtxt("../Datasets_PRML_A1/train1000.txt", delimiter=',', dtype=None, encoding=None)
-data_1 = [list(np.fromstring(data_1[i], dtype=float, count=3, sep=' ')) for i in range(len(data_1))]
+data = np.loadtxt("../Datasets_PRML_A1/train1000.txt", delimiter=' ', dtype=None)
 
-# find w and plot
+X_train = data[:,:2]
+y_train = data[:,-1]
+w = []
+X_poly = getPolyfeatures(X_train, 3)
+w = getAllWeights(X_poly, y_train, 0)
+y_pred = getAllPolyfit(X_poly, np.transpose(w))
+# compute rms error
