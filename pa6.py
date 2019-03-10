f44e36a826a0e8a93cc8778ffbec1e632d68c62f
avlakshmy <avlakshmy@gmail.com>
2019-03-10 10:58:35 +0530

    almost done but need to check histogram

--- pa6.py
+++ pa6.py
@@ -7,10 +7,7 @@ import matplotlib.pyplot as plt
 random.seed(42)
 
 def func(x):
-    return math.exp(2*math.pi*x) - x
-
-def noise(x):
-    return f.eval1DGaussian(0, math.sqrt(0.2))
+    return math.exp(np.tanh(2*math.pi*x)) - x
 
 def eval(w, x):
     sum = 0
@@ -24,26 +21,34 @@ def norm(w):
         norm_val += w[i]*w[i]
     return norm_val
 
+def getPolyfeatures(X,n) :
+    return np.squeeze(np.transpose(np.array([X**i for i in range(n+1)])))
+
+def getPolyfit(X,w) :
+    return np.matmul(X,w)
+
+def getWeights(X,y,ridge) :
+    return np.matmul(np.matmul(np.linalg.inv(ridge*np.eye(X.shape[1]) + np.matmul(np.transpose(X),X)),np.transpose(X)),y)
+
+def getEmpiricalRisk(y_train, y_pred):
+    diff = y_train - y_pred
+    sqd = np.squeeze(np.array([x*x for x in diff]))
+    return sum(sqd)
+
 degrees = [1, 5, 9]
 lambdas = [0.001, 0.01, 0.1]
+
 for d in degrees:
-    w = []
-    # find w using (phi(x)' * phi(x))^-1 * phi(x)' * y
-    # here, I have put some dummy
-    for i in range(d):
-        w.append(random.randint(1,101))
     for myLambda in lambdas:
-        sums_y = []
+        empirical_risks = []
         for i in range(1000):
             samples = []
-            for i in range(10):
-                samples.append(2 * np.random.random_sample() - 1)
-            for i in range(10):
-                sum = 0
-                temp = eval(w, samples[i])
-                sum += temp*temp
-                sum = sum/2.0
-                sum += myLambda*norm(w)
-                sums_y.append(sum)
-    plt.hist(sums_y, bins=10,alpha=0.5)
+            X = np.random.uniform(low=-1,high=1,size=10).reshape(-1,1)
+            y = np.exp(np.tanh(2*math.pi*X)) - X + np.random.normal(0,0.2)
+            X_poly = getPolyfeatures(X, d)
+            w = getWeights(X_poly, y, myLambda)
+            y_pred = getPolyfit(X_poly, w)
+            er = getEmpiricalRisk(y, y_pred)
+            empirical_risks.append(er)
+    plt.hist(empirical_risks, bins=50,alpha=0.5)
     plt.show()
