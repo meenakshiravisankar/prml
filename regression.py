db1fc3b1638f601129b6d55f4b5baefa308aca9c
Meenu5 <ravisankarmeenakshi@gmail.com>
2019-03-14 14:16:16 +0530

    Added plots

--- regression.py
+++ regression.py
@@ -23,6 +23,12 @@ def getPolyfeatures(X,n) :
 def getRMSE(ytrue,ypred,ridge,w) :
     return np.sqrt((np.sum(np.multiply(ytrue-ypred,ytrue-ypred)) + ridge*np.sum(np.multiply(w,w)))/ytrue.shape[0])
     
+def getResults(X,y,degree,ridge) :
+    X = getPolyfeatures(X, degree)
+    w = getWeights(X, y, ridge)
+    y_pred = getPolyfit(X,w)
+    rmse = getRMSE(y,y_pred,ridge,w)
+    return w, y_pred, rmse
 
 # Seeding
 np.random.seed(42)
@@ -56,7 +62,7 @@ y_test = y[train_size+val_size:train_size+val_size+test_size]
 
 # Settings
 degrees = [1,3,6,9]
-ridges = [10**(-17+x) for x in range(18)]
+ridges = [10**(-13+x) for x in range(14)]
 ridges[0] = 0
 sizes = [10,20,30,40,50,60,70]
 all_degrees = np.linspace(0,9,10).astype(int)
@@ -65,7 +71,8 @@ all_degrees = np.linspace(0,9,10).astype(int)
 fig = 221
 rmses = []
 
-default = 1
+# set default to 1 if plots needed without regularisation
+default = 0
 if default :
     ridges = [0]
     sizes = [10]
@@ -76,10 +83,11 @@ for degree in degrees :
         for size in sizes : 
             X_train_new = X_train[:size]            
             y_train_new = y_train[:size]
-            X_train_new_poly = getPolyfeatures(X_train_new, degree)
-            w = getWeights(X_train_new_poly, y_train_new, ridge)
-            y_pred_new = getPolyfit(X_train_new_poly,w)
-            rmse_train = getRMSE(y_train_new, y_pred_new, ridge, w)
+            # X_train_new_poly = getPolyfeatures(X_train_new, degree)
+            # w = getWeights(X_train_new_poly, y_train_new, ridge)
+            # y_pred_new = getPolyfit(X_train_new_poly,w)
+            # rmse_train = getRMSE(y_train_new, y_pred_new, ridge, w)
+            w, y_pred_new, rmse = getResults(X_train_new, y_train_new, degree, ridge)
             rmse_val = getRMSE(y_val,getPolyfit(getPolyfeatures(X_val,degree),w), ridge, w)
             rmses.append(np.array([degree, ridge, size, rmse_val]))
 
@@ -90,22 +98,26 @@ for degree in degrees :
                     plt.xlabel("x")
                 else :
                     plt.ylabel("target")
+
+                # Training data
                 plt.plot(X_train_new,y_train_new,'.')
+                # Curve fit
                 x = np.linspace(0,1,100)
                 x_poly = getPolyfeatures(x,degree)
                 y_func = getPolyfit(x_poly,w)
+                # Coefficients
                 np.savetxt("results/coefficients/"+str(degree)+".txt",w,newline=" ")
                 M = "M="+str(degree)
                 plt.plot(x, y_func,label=M)
-                # plt.xlim(0,1)
-                # plt.ylim(0,4)
                 plt.legend()
-
-                figure.suptitle("Data, Target, Regression Output")
+                figure.suptitle("Data, Target, Regression Output (without regularisation)")
                 plt.savefig("results/targetvsx.png")
 
-best_model = rmses[np.argmin(np.array(rmses),axis=0)[-1]]
-print("Parameters of best model has degree {:.2f}, lambda {:.2f}, train size {:.2f}".format(best_model[0],best_model[1],best_model[2]))
+# Finding best model based using validation set
+rmses = np.array(rmses)
+scores = rmses[:,-1].reshape(-1,1)
+best_model = rmses[np.argmin(scores,axis=0)[0]]
+print("Parameters of best model has degree, lambda, train size",best_model[0],best_model[1],best_model[2])
 
 if default :
     raise SystemExit
@@ -115,13 +127,23 @@ deg1 = []
 rmse2 = []
 deg2 = []
 
+# rmse1 - with ridge regularisation and train size 10, rmse2 - without ridge regression and vary train size
+mini1 = 10
+mini2 = 10
 for rmse in rmses :
     if rmse[2] == 10 and rmse[1] !=0 :
         rmse1.append(rmse[3])
         deg1.append(rmse[0])
+        if mini1 > rmse[3] :
+            mini1 = rmse[3]
+            best_ridge = rmse[1]
     if rmse[1] == 0 :
         rmse2.append(rmse[3])
         deg2.append(rmse[0])
+        if mini2 > rmse[3] :
+            mini2 = rmse[3]
+            best_size = rmse[2]
+
 
 plt.rcParams.update({'font.size': 8})
 plt.subplot(221)
@@ -151,27 +173,26 @@ plt.title("Val RMS Error vs Train Size (No reg)",fontsize=8)
 plt.legend(fontsize=8, loc="upper right")
 
 plt.subplot(223)
-
+# Target vs output plot
 size = best_model[2].astype(int)
 ridge = best_model[1]
 degree = best_model[0].astype(int)
 
 X_train_new = X_train[:size]
 y_train_new = y_train[:size]
-X_train_new_poly = getPolyfeatures(X_train_new, degree)
-w = getWeights(X_train_new_poly, y_train_new, ridge)
-y_pred_new = getPolyfit(X_train_new_poly,w)
+w, y_pred_new, rmse = getResults(X_train_new, y_train_new, degree, ridge)
 y_pred_test = getPolyfit(getPolyfeatures(X_test,degree),w)
 
 plt.plot(y_train_new, y_pred_new,"*",label="Train")
 plt.plot(y_test, y_pred_test,"+",label="Test")
 plt.xlabel("True target",fontsize=8)
 plt.ylabel("Model output",fontsize=8)
-# plt.title("Output vs target", fontsize=8)
 plt.legend(fontsize=8)
 
+
+# Plotting Erms for train and test data without any regularisation
 ridge = 0
-size = 70
+size = 10
 X_train_new = X_train[:size]
 y_train_new = y_train[:size]
 
@@ -179,14 +200,9 @@ rmse_train = []
 rmse_test = [] 
 
 for degree in all_degrees : 
-    X_train_new_poly = getPolyfeatures(X_train_new, degree)
-    # print(X_train_new_poly.shape)
-    w = getWeights(X_train_new_poly, y_train_new, ridge)
-    y_pred_new = getPolyfit(X_train_new_poly,w)
-    # plt.plot(y_pred_new, y_train_new, "*")
-    rmse_train.append(getRMSE(y_train_new, y_pred_new, ridge, w))
+    w, y_pred_new, rmse = getResults(X_train_new, y_train_new, degree, ridge)
+    rmse_train.append(rmse)
     rmse_test.append(getRMSE(y_test,getPolyfit(getPolyfeatures(X_test,degree),w), ridge, w))
-
 
 plt.subplot(224)
 plt.tight_layout()
@@ -194,4 +210,49 @@ plt.plot(all_degrees,rmse_train,"-*",label="Train")
 plt.plot(all_degrees,rmse_test,"-*",label="Test")
 plt.legend(fontsize=8,loc="upper right")
 plt.savefig("results/erms.png")
+
+
+# Analysing overfitting case
+figure = plt.figure()
+degree = 9
+size = 10
+
+plt.subplot(231)
+x = np.linspace(0,1,100)
+x_poly = getPolyfeatures(x,degree)
+plt.plot(X_train_new,y_train_new,'.')
+# with ridge regression for 9th degree polynomial
+X_train_new = X_train[:size]
+y_train_new = y_train[:size]
+w, y_pred_new, rmse = getResults(X_train_new, y_train_new, degree, best_ridge)
+y_func = getPolyfit(x_poly,w)
+plt.plot(x, y_func)
+plt.title(r'$\lambda=$'+str(best_ridge))
+plt.ylabel("target")
+plt.tight_layout()
+
+plt.subplot(232)
+# with regularisation using large train size for 9th degree polynomial
+X_train_new = X_train[:int(best_size)]
+y_train_new = y_train[:int(best_size)]
+plt.plot(X_train_new,y_train_new,'.')
+w, y_pred_new, rmse = getResults(X_train_new, y_train_new, degree, 0)
+y_func = getPolyfit(x_poly,w)
+plt.plot(x, y_func)
+plt.title("Train size - "+str(int(best_size)))
+plt.tight_layout()
+
+plt.subplot(233)
+# without regularisation for 9th degree polynomial
+X_train_new = X_train[:10]
+y_train_new = y_train[:10]
+plt.plot(X_train_new,y_train_new,'.')
+w, y_pred_new, rmse = getResults(X_train_new, y_train_new, degree, 0)
+y_func = getPolyfit(x_poly,w)
+plt.plot(x, y_func)
+plt.title("No regularisation")
+plt.xlabel("x")
+plt.tight_layout()
+# figure.suptitle("Analysis of Overfitting for degree 9")
+
 plt.show()
