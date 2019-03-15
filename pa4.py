2434df0edb71a58bf90327598914513bcc5712e0
avlakshmy <avlakshmy@gmail.com>
2019-03-15 14:02:31 +0530

    added comments to code; modified plot to have same scale on y-axis

--- pa4.py
+++ pa4.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
+# seeding
 np.random.seed(42)
 
+# function to evaluate normal function
 def eval1DGaussian(X, mu, sigma):
     power = (np.sum((X-mu)*(X-mu)) * -1) / (2 * sigma * sigma)
     factor = 1/(np.sqrt(2 * np.pi) * sigma)
@@ -14,10 +16,17 @@ def eval1DGaussian(X, mu, sigma):
 data = np.loadtxt("../Datasets_PRML_A1/Dataset_5_Team_39.csv", delimiter=',', dtype=None)
 mu0 = -1
 
+# fixing the x-axis limits for the plots
 x_plot = np.linspace(-25, 25, 1000)
+
+# range of values taken by (sigma^2/sigma_0^2)
 factors = [0.1, 1, 10, 100]
+
+# range of dataset sizes
 dataset_sizes = [10, 100, 1000]
+
 fig = 221
+
 for factor in factors:
     mu_n = []
     sigma_estimates = []
@@ -32,6 +41,8 @@ for factor in factors:
         X = data[0:n]
         Sn = sum(X)
         Sn2 = sum(X*X)
+
+        # posterior mean of mu
         mu = (Sn + mu0*factor) / (n + factor)
         mu_n.append(mu)
 
@@ -39,13 +50,14 @@ for factor in factors:
         sigma = (Sn2 - 2*Sn*mu + mu*mu) / n
         sigma_estimates.append(sigma)
 
+    # plotting the posterior density
     for i in range(3):
         labelname = "n = " + str(dataset_sizes[i])
         plt.plot(x_plot, stats.norm.pdf(x_plot, mu_n[i], sigma_estimates[i]), label = labelname)
+        plt.ylim(top=0.4)
         if fig == 223 :
             plt.legend(fontsize=8, loc='upper right')
-# plt.legend()
-# plt.figlegend(labels=['n=10','n=100','n=1000'], loc='center center')
+
 plt.suptitle("Estimated Density P(x/D)",fontsize=8)
 plt.savefig("results/q4.png")
 plt.show()
