330b5c119d4589a247df16eda07e5aa863a733d3
meenakshiravisankar <ravisankarmeenakshi@gmail.com>
2019-04-03 17:29:04 +0530

    added todo

--- PA2/image_plot.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from matplotlib import pyplot as plt
-
-img_array = np.load('../Datasets_PRML_A2/Image_Dataset/fan.npy')
-plt.imshow(img_array, cmap='gray')
-plt.savefig("results/fan.png")
--- PA2/scatter_plots.py
+++ PA2/scatter_plots.py
@@ -1,27 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+classes = [0,1]
 f = plt.figure(1)
 data1 = np.loadtxt("../Datasets_PRML_A2/Dataset_1_Team_39.csv", delimiter=',', dtype=None)
-plt.scatter(data1[:,0],data1[:,1],c=data1[:,2])
+for class_val in classes :
+    idx = (data1[:,2]==class_val)
+    plt.scatter(data1[idx,0],data1[idx,1],label="class "+str(class_val),alpha=0.7)
+plt.legend()
 plt.savefig("results/Dataset1_scatter")
 
 f = plt.figure(2)
 data2 = np.loadtxt("../Datasets_PRML_A2/Dataset_2_Team_39.csv", delimiter=',', dtype=None)
-plt.scatter(data2[:,0],data2[:,1],c=data2[:,2])
+for class_val in classes :
+    idx = (data2[:,2]==class_val)
+    plt.scatter(data2[idx,0],data2[idx,1],label="class "+str(class_val),alpha=0.7)
+plt.legend()
 plt.savefig("results/Dataset2_scatter")
 
 f = plt.figure(3)
 data3 = np.loadtxt("../Datasets_PRML_A2/Dataset_3_Team_39.csv", delimiter=',', dtype=None)
-plt.scatter(data3[:,0],data3[:,1],c=data3[:,2])
+for class_val in classes :
+    idx = (data3[:,2]==class_val)
+    plt.scatter(data3[idx,0],data3[idx,1],label="class "+str(class_val),alpha=0.7)
+plt.legend()
 plt.savefig("results/Dataset3_scatter")
 
 f = plt.figure(4)
 data4 = np.loadtxt("../Datasets_PRML_A2/Dataset_4_Team_39.csv", delimiter=',', dtype=None)
-plt.scatter(data4[:,0],data4[:,1],c=data4[:,2])
+for class_val in classes :
+    idx = (data4[:,2]==class_val)
+    plt.scatter(data4[idx,0],data4[idx,1],label="class "+str(class_val),alpha=0.7)
+plt.legend()
 plt.savefig("results/Dataset4_scatter")
+
 
 f = plt.figure(5)
 data5 = np.loadtxt("../Datasets_PRML_A2/Dataset_5_Team_39.csv", delimiter=',', dtype=None)
-plt.scatter(data5[:,0],data5[:,1],c=data5[:,2])
+for class_val in classes :
+    idx = (data5[:,2]==class_val)
+    plt.scatter(data5[idx,0],data5[idx,1],label="class "+str(class_val),alpha=0.7)
+plt.legend()
 plt.savefig("results/Dataset5_scatter")
